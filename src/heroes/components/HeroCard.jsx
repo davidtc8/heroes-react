3fc01dfe9fb3c321b562b12bca2415a517a09ef5
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-12 07:29:51 -0600

    Hero Card Component

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const heroImageURL = `/assets/heroes/${id}.jpg`;
+
+  const charactersByHero = <p>{characters}</p>;
+
+  return (
+    <div className="col animate__animated animate__bounceInUp">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img src={heroImageURL} alt={superhero} className="card-img" />
+          </div>
+
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+              
+              {
+              (alter_ego === characters) ? <></> : charactersByHero
+              }
+              
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>More</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
