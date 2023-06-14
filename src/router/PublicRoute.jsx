85cd759cc2f38bb8473c531eb61b64b9818fadee
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-14 12:02:33 -0600

    Public route in React

---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,14 @@
+import React, { Children, useContext } from 'react'
+import { AuthContext } from '../auth'
+import { Navigate } from 'react-router-dom'
+
+export const PublicRoute = ({ children }) => {
+
+    const { logged } = useContext( AuthContext )
+
+  return (
+    (!logged)
+    ? children
+    : <Navigate to='/marvel' />
+  )
+}
