0574bf893b65cc0392bfd9e671d72915a1f9b607
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-14 12:02:48 -0600

    Put the new username in the NavBar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import {AuthContext} from '../../auth/context/AuthContext'
 
 export const Navbar = () => {
+
+  const { user, logout } = useContext( AuthContext );
 
   // Este es un custom hook creado por la gente de react-router-dom
   const navigate = useNavigate();
 
   const onLogout = () => {
+    logout();
     navigate('/login', {
       // El replace evita que la persona pueda regresar al historial anterior
       replace: true
@@ -43,7 +48,7 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">
-            David
+            { user?.name }
           </span>
           <button className="nav-item nav-link btn" onClick={onLogout}>
             Logout
