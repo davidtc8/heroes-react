c1d05741ecc9af39e1d86e355f474ecd38e07321
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-14 19:45:36 -0600

    Improved HeroCard Component

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import '../styles/heropage.css'
 
 export const HeroCard = ({
   id,
@@ -10,25 +11,28 @@ export const HeroCard = ({
 }) => {
   const heroImageURL = `/heroes/${id}.jpg`;
 
-  const charactersByHero = <p>{characters}</p>;
+  const CharactersByHero = ({ alter_ego, characters }) => {
+    // if ( alter_ego === characters ) return (<></>);
+    // return <p>{ characters }</p>
+    return alter_ego === characters ? <></> : <p>{characters}</p>;
+  };
 
   return (
     <div className="col animate__animated animate__bounceInUp">
-      <div className="card">
+      <div className="card card_width_responsive">
         <div className="row no-gutters">
           <div className="col-4">
-            <img src={heroImageURL} alt={superhero} className="card-img" />
+            <img src={heroImageURL} alt={superhero} className="card-img img_size text" />
           </div>
 
-          <div className="col-8">
+          <div className="col-8 my_col">
             <div className="card-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
-              
-              {
-              (alter_ego === characters) ? <></> : charactersByHero
-              }
-              
+
+              {/* {alter_ego === characters ? <></> : charactersByHero} */}
+              <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
+
               <p className="card-text">
                 <small className="text-muted">{first_appearance}</small>
               </p>
